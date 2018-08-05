e5a124a7f7482ce57fd212b7a5660a2482eebbb1
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 02:44:41 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,9 +1,11 @@
 class Student
   attr_accessor :name, :grade
-  
-  def initialize(name:, grade:)
+
+  def initialize(name:, grade:, id: = nil)
     @name = name
     @grade = grade
   end
+
+  def 
 
 end
