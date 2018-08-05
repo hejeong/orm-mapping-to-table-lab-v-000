01664ed4c6a7ddc3ae2edc7caf6f741cba28c52e
Jonathan Hong <jonhong777@gmail.com>
2018-08-05 02:58:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -22,4 +22,11 @@ class Student
     DB[:conn].execute(sql)
   end
 
+  def save(name, grade)
+    sql = <<-SQL
+          INSERT INTO students (name, grade) VALUES (?,?)
+          SQL
+    DB[:conn].execute(sql, name, grade)
+  end
+
 end
